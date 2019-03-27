644d1d3bba0cf92d04da2fe6bdad16125e1794a1
LAPTOP-7QKHCA95\david <david.velasquezm@upb.edu.co>
2019-03-27 14:48:29 -0500

    intentando login

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// JavaScript source code
+$("#btnLogin").click(function login() {
+    var userEmail = $("#user").val();
+    var userPass = $("#pass").val();
+    window.alert(userEmail + " " + userPass);
+});
+
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyDJS6peY9rslv81SoN2Q08JcSTnVFYD_2E",
+        authDomain: "construapp-v1.firebaseapp.com",
+        databaseURL: "https://construapp-v1.firebaseio.com",
+        projectId: "construapp-v1",
+        storageBucket: "construapp-v1.appspot.com",
+        messagingSenderId: "312248850383"
+      };
+      firebase.initializeApp(config);
+
