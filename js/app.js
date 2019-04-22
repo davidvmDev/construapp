12938cfd0902d6fbb5ba3b3220a947d3164bb0ff
LAPTOP-7QKHCA95\david <david.velasquezm@upb.edu.co>
2019-04-22 14:52:10 -0500

    cambio estructural Framework7
    se cambio la esteuctura del framework

---
+++ js/app.js
@@ -0,0 +1,89 @@
+var config = {
+    apiKey: "AIzaSyDJS6peY9rslv81SoN2Q08JcSTnVFYD_2E",
+    authDomain: "construapp-v1.firebaseapp.com",
+    databaseURL: "https://construapp-v1.firebaseio.com",
+    projectId: "construapp-v1",
+    storageBucket: "construapp-v1.appspot.com",
+    messagingSenderId: "312248850383"
+};
+firebase.initializeApp(config);
+$("#cerrar").click(function logout() {
+    firebase.auth().signOut().then(function () {
+        location.href = "index.html";
+    }).catch(function (error) {
+        // An error happened.
+    });
+});
+
+
+// Dom7
+var $$ = Dom7;
+
+
+
+
+// Framework7 App main instance
+var app  = new Framework7({
+  root: '#app', // App root element
+  id: 'io.framework7.testapp', // App bundle ID
+  name: 'Framework7', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data: function () {
+    return {
+      user: {
+        firstName: 'John',
+        lastName: 'Doe',
+      },
+      // Demo products for Catalog section
+      products: [
+        {
+          titulo: '1',
+          title: 'Apple iPhone 8',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+        },
+        {
+          id: '2',
+          title: 'Apple iPhone 8 Plus',
+          description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+        },
+        {
+          id: '3',
+          title: 'Apple iPhone X',
+          description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+        },
+      ]
+    };
+  },
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+  // App routes
+  routes: routes,
+});
+
+
+
+// Init/Create views
+var homeView = app.views.create('#view-home', {
+  url: '/'
+});
+var catalogView = app.views.create('#view-catalog', {
+  url: '/catalog/'
+});
+var settingsView = app.views.create('#view-settings', {
+  url: '/settings/'
+});
+
+
+$$('#price-filter').on('range:change', function (e, range) {
+    $$('.price-value').text('$' + (range.value[0]) + ' - $' + (range.value[1]));
+});
+
+
+
+
+
---
+++ js/filtro.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+
+    
+    function ejemplo() {
+            var mensaje = "<strong>Un saludo</strong>";
+    document.getElementById("ej").innerHTML = mensaje;
+}
+function ejemplo2() {
+    var mensaje = "<strong>Un saludo</strong>";
+    document.getElementById("ej2").innerHTML = mensaje;
+}
+
+
