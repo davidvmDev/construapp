23294888572f2276203e26850fdc3a99c4b07d23
LAPTOP-7QKHCA95\david <david.velasquezm@upb.edu.co>
2019-04-29 13:56:53 -0500

    catalogo

---
+++ js/catalogo.js
@@ -0,0 +1,94 @@
+// JavaScript source code
+catalogo: {
+    cemento: {
+        products: [
+            {   
+                id: '1',
+                titulo: 'Cemento x 50kg',
+                peso: '50',
+                marca: 'Argos',
+                deposito: 'Miranda',
+                imgP: img/miranda.jpg,
+                imgG: img / Pcemento.jpg,
+                descripcion: 'El cemento es un material que resulta de la combinación de arcilla molida con materiales calcáreos de polvo, en tanto, una vez que entran en contacto con el agua se solidifica y vuelve duro. Es mayormente empleado a instancias de la construcción, justamente por esa solidez que reviste, como adherente y aglutinante.',
+                precio:'22000',
+                tipo:'Bulto'
+                
+            },
+            {
+                id: '2',
+                titulo: 'Cemento x 50kg',
+                peso: '50',
+                marca: 'Cemex',
+                deposito: 'Para construir',
+                imgP: img / miranda.jpg,
+                imgG: img / Pcemento.jpg,
+                descripcion: 'El cemento es un material que resulta de la combinación de arcilla molida con materiales calcáreos de polvo, en tanto, una vez que entran en contacto con el agua se solidifica y vuelve duro. Es mayormente empleado a instancias de la construcción, justamente por esa solidez que reviste, como adherente y aglutinante.',
+                precio: '20000',
+                tipo: 'Bulto'
+            },
+            {
+                id: '3',
+                titulo: 'Cemento x 25kg',
+                peso: '25',
+                marca: 'Argos',
+                deposito: 'Titan',
+                imgP: img/titan.jpg,
+                imgG: img/Pcemento.jpg,
+                descripcion: 'El cemento es un material que resulta de la combinación de arcilla molida con materiales calcáreos de polvo, en tanto, una vez que entran en contacto con el agua se solidifica y vuelve duro. Es mayormente empleado a instancias de la construcción, justamente por esa solidez que reviste, como adherente y aglutinante.',
+                precio: '16000',
+                tipo: 'Bulto'
+            },
+            {
+                id: '4',
+                titulo: 'Cemento x 25kg',
+                peso: '25',
+                marca: 'Diamante',
+                deposito: 'Botero',
+                imgP: img/botero.jpg,
+                imgG: img/Pcemento.jpg,
+                descripcion: 'El cemento es un material que resulta de la combinación de arcilla molida con materiales calcáreos de polvo, en tanto, una vez que entran en contacto con el agua se solidifica y vuelve duro. Es mayormente empleado a instancias de la construcción, justamente por esa solidez que reviste, como adherente y aglutinante.',
+                precio: '15000',
+                tipo: 'Bulto'
+            },
+            {
+                id: '5',
+                titulo: 'Cemento x 50kg',
+                peso: '50',
+                marca: 'Diamante',
+                deposito: 'medina',
+                imgP: img / medina.jpg,
+                imgG: img / Pcemento.jpg,
+                descripcion: 'El cemento es un material que resulta de la combinación de arcilla molida con materiales calcáreos de polvo, en tanto, una vez que entran en contacto con el agua se solidifica y vuelve duro. Es mayormente empleado a instancias de la construcción, justamente por esa solidez que reviste, como adherente y aglutinante.',
+                precio: '19000',
+                tipo: 'Bulto'
+            },
+            {
+                id: '6',
+                titulo: 'Cemento x 50kg',
+                peso: '50',
+                marca: 'Argos',
+                deposito: 'San pio',
+                imgP: img / sanpio.jpg,
+                imgG: img / Pcemento.jpg,
+                descripcion: 'El cemento es un material que resulta de la combinación de arcilla molida con materiales calcáreos de polvo, en tanto, una vez que entran en contacto con el agua se solidifica y vuelve duro. Es mayormente empleado a instancias de la construcción, justamente por esa solidez que reviste, como adherente y aglutinante.',
+                precio: '23000',
+                tipo: 'Bulto'
+            },
+            {
+                id: '7',
+                titulo: 'Cemento x 50kg',
+                peso: '50',
+                marca: 'Topex',
+                deposito: 'Paris',
+                imgP: img /paris.jpg,
+                imgG: img / Pcemento.jpg,
+                descripcion: 'El cemento es un material que resulta de la combinación de arcilla molida con materiales calcáreos de polvo, en tanto, una vez que entran en contacto con el agua se solidifica y vuelve duro. Es mayormente empleado a instancias de la construcción, justamente por esa solidez que reviste, como adherente y aglutinante.',
+                precio: '16000',
+                tipo: 'Bulto'
+              
+
+            },
+        ]
+    }
+}
